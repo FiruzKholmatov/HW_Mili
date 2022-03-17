5d47e555ddef80cfbac4ba4298ac77c225137b26
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-03-17 14:12:58 +0300

    files added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 18_540;
+        int miles = ticketPrice / 20;
+
+
+        System.out.println("=========================");
+        System.out.println("Ваши бонусные мили: " + miles);
+        System.out.println("=========================");
+
+
+    }
+}
